1f938912f5338c71e6fb1e3b3ee3d6846ec34033
mishyTheBear <hamish.dickson@gmail.com>
2013-12-21 19:22:19 +0000

    And yet more logging changes
    This really is the last one tonight - I've been told to put my laptop
    down

--- logger/figdocLogger.py
+++ logger/figdocLogger.py
@@ -10,10 +10,17 @@ __author__ = 'DicksonH'
 # FigdocLogger.write_info should be used when recording normal operation
 # FigdocLogger.write_warning should be used when recording something may cause incorrect data coming out
 # FigdocLogger.write_error should be used when something bad happens and the form doesn't run
-
+# FigdocLogger.write_to_log is just a general wrapper
 
 # So it turns out that that core python logging class is a bit odd, in that it's designed so you don't subclass it,
 # the idea is that instead you can call it from lots of different places and it uses the same log
+
+# Yes, I too have read that you shouldn't really use static methods in python and it's something only java programmers
+# try to use .. but I've done this so I can use __init__ to set up everything .. I may change my mind about all this
+# later ... but right now it makes sense
+
+# @TODO: check to see if using the write log, if not .. create a new one
+# @TODO: basically do all the iseries work!
 
 import logging
 import time
@@ -24,17 +31,34 @@ import figconfig as figConfig
 
 class FigdocLogger():
 
+    # static variables ... creates a log even if nothing is configured .. don't unstatic (that's totally a word)
     _log_to_i = False
     _daily_log = False
     _local_file_location = "logs"
     _local_file_name = "localLog.log"
     _is_instance = False
 
+    INFO = "info"
+    WARNING = "warning"
+    ERROR = "error"
+
+    # This gets set on when a write to the i failed ... switched off whenever a successful write happens
+    _failed_i_write_warning = False
+
     @staticmethod
     def _get_datetime():
         ts = time.time()
         st = "" + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') + ": "
         return st
+
+    @staticmethod
+    def _failed_i_write(msg):
+        # only come in here if the write to the i failed
+        if not FigdocLogger._failed_i_write_warning:
+            FigdocLogger._failed_i_write_warning = True
+            logging.error(FigdocLogger._get_datetime() + "Failed write to iSeries")
+
+        logging.info(FigdocLogger._get_datetime() + msg)
 
     def __init__(self):
         if not FigdocLogger._is_instance:
@@ -53,7 +77,7 @@ class FigdocLogger():
                 # use the hard coded config
 
             # create the log/connection
-            if self._log_to_i:
+            if FigdocLogger._log_to_i:
                 try:
                     print "write here and say process is starting up"
                 except:
@@ -62,56 +86,72 @@ class FigdocLogger():
                     if not os.path.exists(FigdocLogger._local_file_location):
                         os.makedirs(FigdocLogger._local_file_location)
 
+                    print "Writing log: " + self.file
+
                     logging.basicConfig(filename=self.file, level=logging.DEBUG)
                     logging.info("=========================================================================")
-                    logging.info(self._get_datetime() + "STARTING LOG")
-                    logging.exception(self._get_datetime() + "Failed attempt to write to i")
+                    logging.info(FigdocLogger._get_datetime() + "STARTING LOG")
+                    logging.exception(FigdocLogger._get_datetime() + "Failed attempt to write to i")
                     logging.info("=========================================================================")
             else:
                 self.file = FigdocLogger._local_file_location + "\\" + FigdocLogger._local_file_name
                 if not os.path.exists(FigdocLogger._local_file_location):
                     os.makedirs(FigdocLogger._local_file_location)
 
+                print "Writing log: " + self.file
+
                 logging.basicConfig(filename=self.file, level=logging.DEBUG)
                 logging.info("=========================================================================")
-                logging.info(self._get_datetime() + "STARTING LOG")
+                logging.info(FigdocLogger._get_datetime() + "STARTING LOG")
                 logging.info("=========================================================================")
 
-    def write_info(self, msg):
-        if self._log_to_i:
-            try:
-                # connect to the iseries
-                print "logging to i"
-            except:
-                logging.exception("Couldn't write to iSeries")
-                logging.info(self._get_datetime() + msg)
+    @staticmethod
+    def write_to_log(msg, level=None):
+        if level is None or level == FigdocLogger.INFO:
+            FigdocLogger.write_info(msg)
+        elif level == FigdocLogger.WARNING:
+            FigdocLogger.write_warning(msg)
+        elif level == FigdocLogger.ERROR:
+            FigdocLogger.write_error(msg)
         else:
-            # log locally
-            logging.info(self._get_datetime() + msg)
+            FigdocLogger.write_error("Bad logging message")
 
-    def write_warning(self, msg):
-        if self._log_to_i:
+    @staticmethod
+    def write_info(msg):
+        if FigdocLogger._log_to_i:
             try:
                 # connect to the iseries
                 print "logging to i"
             except:
-                logging.exception("Couldn't write to iSeries")
-                logging.warning(self._get_datetime() + msg)
+                FigdocLogger._failed_i_write(msg)
         else:
             # log locally
-            logging.warning(self._get_datetime() + msg)
+            logging.info(FigdocLogger._get_datetime() + msg)
 
-    def write_error(self, msg):
-        if self._log_to_i:
+    @staticmethod
+    def write_warning(msg):
+        if FigdocLogger._log_to_i:
             try:
                 # connect to the iseries
                 print "logging to i"
             except:
-                logging.exception("Couldn't write to iSeries")
-                logging.error(self._get_datetime() + msg)
+                FigdocLogger._failed_i_write(msg)
         else:
             # log locally
-            logging.error(self._get_datetime() + msg)
+            logging.warning(FigdocLogger._get_datetime() + msg)
+
+    @staticmethod
+    def write_error(msg):
+        if FigdocLogger._log_to_i:
+            try:
+                # connect to the iseries
+                print "logging to i"
+            except:
+                FigdocLogger._failed_i_write(msg)
+        else:
+            # log locally
+            logging.error(FigdocLogger._get_datetime() + msg)
+
 
 # quick test stub ...
 if __name__ == '__main__':
@@ -122,4 +162,4 @@ if __name__ == '__main__':
     b.write_info("second instance")
 
     a.write_warning("this is a warning")
-    b.write_catastrophe("arrgh! stuff broke!!")
+    b.write_error("arrgh! stuff broke!!")
